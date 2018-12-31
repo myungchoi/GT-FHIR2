5c2a53fd9176e9fb4aac11a721d56c98e777e90d
Myung Choi <myungchoi@gmail.com>
2018-12-31 12:26:06 -0500

    Apache copyright added and jpa has separate POM with new version

--- omoponfhir-omopv5-jpabase/src/main/java/edu/gatech/chai/omopv5/jpa/enity/noomop/package-info.java
+++
@@ -1,3 +0,0 @@
-@org.hibernate.annotations.TypeDef(name = "FhirJsonType", typeClass = FhirJsonType.class)
-
-package edu.gatech.chai.omopv5.jpa.enity.noomop;
--- omoponfhir-omopv5-jpabase/src/main/java/edu/gatech/chai/omopv5/jpa/service/CareSiteServiceImp.java
+++ omoponfhir-omopv5-jpabase/src/main/java/edu/gatech/chai/omopv5/jpa/service/CareSiteServiceImp.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Georgia Tech Research Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/
 package edu.gatech.chai.omopv5.jpa.service;
 
 import java.util.List;
@@ -20,7 +36,6 @@ public class CareSiteServiceImp extends BaseEntityServiceImp<CareSite, CareSiteD
 	}
 	
 	@Transactional(readOnly = true)
-	@Override
 	public CareSite searchByLocation(Location location) {
 		EntityManager em = getEntityDao().getEntityManager();
 		String query = "SELECT t FROM CareSite t WHERE location_id like :value:";
@@ -33,7 +48,6 @@ public class CareSiteServiceImp extends BaseEntityServiceImp<CareSite, CareSiteD
 	}
 
 	@Transactional(readOnly = true)
-	@Override
 	public CareSite searchByNameAndLocation(String careSiteName, Location location) {
 		EntityManager em = getEntityDao().getEntityManager();
 		String queryString = "SELECT t FROM CareSite t WHERE";
